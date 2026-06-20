c0157ed6217bca0a149999f1fc2f529ef2e0f563
ych19860118-hash <ych19860118@gmail.com>
2026-06-20 13:18:05 +0800

    Create leaflet.js

---
+++ leaflet.js
@@ -0,0 +1,7 @@
+(function() {
+    if (window.L) return;
+    var script = document.createElement('script');
+    script.src = 'https://unpkg.com';
+    script.crossOrigin = 'anonymous';
+    document.head.appendChild(script);
+})();
